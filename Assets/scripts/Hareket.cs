bcec76e12cd2c634c20e399cc906a3c0a759e4fe
Aatxe <brkcnbdr12@hotmail.com>
2023-11-17 16:58:10 +0300

    hareket cs güncellendi

--- Assets/scripts/Hareket.cs
+++ Assets/scripts/Hareket.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private float horizontal;
-    private float speed = 8f;
-    private float jumpingPower = 16f;
+    public float speed = 8f;
+    public float jumpingPower = 16f;
     private bool isFacingRight = true;
 
     private bool canDash = true;
